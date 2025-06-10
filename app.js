4848c840614abfc482245fb6d6cfd17e59be74b8
Murilo Ferreira Santos <murferdev@gmail.com>
2025-06-09 21:02:54 -0300

    Ajuste no nome dos arquivos

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const mysql = require('mysql2');
 
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
 
-app.engine('hanclebars', engine())
-;
+app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
@@ -15,11 +14,20 @@ const conexao = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '@MySQL_$3n4c#',
-    database: 'DB_Native_Games'
+    database: 'db_native_games'
 })
 
-app.get('/', (req, res) => {
-    res.send('');
-});
+app.get('/usuarios', (req, res) => {;
+    let sql = 'SELECT * FROM tb_usuarios';
+    conexao.query(sql, function(erro, usuarios_qs) {
+        if (erro) {
+            console.error('Erro ao consultar usuarios:', erro);
+            res.status(500).send('Erro ao consultar usuarios');
+            return;
+        }
+        res.render('tb_usuarios', {usuario: usuarios_qs});
+    });
+}
+);
 
 app.listen(8080);
