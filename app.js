1381dcdeb07cd75a8a685d52d8349df095fb0827
Murilo Ferreira Santos <murferdev@gmail.com>
2025-06-14 21:55:30 -0300

    Criação inicial dos arquivos .handlebars

--- app.js
+++ app.js
@@ -1,23 +1,62 @@
+// Importação de módulos necessários
 const express = require('express');
 const app = express();
 const {engine} = require ('express-handlebars');
 
 const mysql = require('mysql2');
 
-app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
-
+// Configuração do middleware para servir arquivos estáticos
+app.use(express.static(__dirname + '/static')); // Pasta para arquivos estáticos
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist')); // Pasta para o Bootstrap
+// Configuração do Handlebars como motor de visualização
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+// Configuração da conexão com o banco de dados MySQL
 const conexao = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'senac',
-    database: 'native_games_DB'
+    password: '@MySQL_$3n4c#',
+    database: 'native_games_db'
 })
 
-app.get('/usuarios', (req, res) => {;
+// Conexão com o banco de dados
+conexao.connect(function(erro) {
+    if (erro) {
+        console.error('Erro ao conectar ao banco de dados:', erro);
+        return;
+    }
+    console.log('Conexão com o banco de dados estabelecida com sucesso!');
+});
+
+// Rota para consultar usuários
+app.get('/', (req, res) => {
+    res.render('index', {titulo: 'Teste de Renderização'});
+});
+
+// Rota para a página "Login"
+app.get('/login', (req, res) => {
+    res.render('login', {titulo: 'Login'});
+});
+
+// Rota para a página "Cadastro"
+app.get('/registrar', (req, res) => {
+    res.render('registrar', {titulo: 'Cadastro de Usuário'});
+});
+
+// Rota para a página "Sobre"
+app.get('/sobre', (req, res) => { 
+    res.render('sobre', {titulo: 'Sobre o Projeto'});
+});
+
+// Rota para a página "Contato"
+app.get('/contato', (req, res) => {
+    res.render('contato', {titulo: 'Fale Conosco'});
+});
+
+// Rota para consultar usuários
+app.get('/usuarios', (req, res) => {; 
     let sql = 'SELECT * FROM tb_usuarios';
     conexao.query(sql, function(erro, usuarios_qs) {
         if (erro) {
@@ -27,7 +66,6 @@ app.get('/usuarios', (req, res) => {;
         }
         res.render('usuarios', {usuarios: usuarios_qs});
     });
-}
-);
+});
 
 app.listen(8080);
