5161358e4fdfc706e24c10ab9e32b9e8e154a1ff
Murilo Ferreira Santos <murferdev@gmail.com>
2025-07-08 23:32:28 -0300

    Criação da página de edição de perfil do usuário "editar_usuario".

--- app.js
+++ app.js
@@ -3,34 +3,52 @@ require('dotenv').config();
 // Importação de módulos necessários
 const express = require('express');
 const app = express();
-const {engine} = require ('express-handlebars');
+const { engine } = require('express-handlebars');
 const mysql = require('mysql2');
 const path = require('path');
+
+// Configuração da conexão com o banco de dados MySQL
+const conexao = mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '@MySQL_$3n4c#',
+    database: process.env.DB_NAME || 'native_games_DB'
+});
+
+conexao.connect(function (erro) {
+    if (erro) {
+        console.error('Erro ao conectar ao banco de dados:', erro);
+        process.exit(1);
+    } else {
+        console.log('Conexão com o banco de dados estabelecida com sucesso!');
+    }
+});
 
 // Configuração do middleware para servir arquivos estáticos
 app.use(express.static(__dirname + '/static')); // Pasta para arquivos estáticos
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist')); // Pasta para o Bootstrap
 app.use(express.urlencoded({ extended: true })); // Middleware para analisar dados de formulários
 // Configuração do Handlebars como motor de visualização
-app.engine('handlebars', engine({ defaultLayout: 'main', 
-      helpers: {
-    ifCond: function (v1, operator, v2, options) {
-      switch (operator) {
-        case '==':
-          return (v1 == v2) ? options.fn(this) : options.inverse(this);
-        case '===':
-          return (v1 === v2) ? options.fn(this) : options.inverse(this);
-        case '!=':
-          return (v1 != v2) ? options.fn(this) : options.inverse(this);
-        case '<':
-          return (v1 < v2) ? options.fn(this) : options.inverse(this);
-        case '>':
-          return (v1 > v2) ? options.fn(this) : options.inverse(this);
-        default:
-          return options.inverse(this);
-      }
+app.engine('handlebars', engine({
+    defaultLayout: 'main',
+    helpers: {
+        ifCond: function (v1, operator, v2, options) {
+            switch (operator) {
+                case '==':
+                    return (v1 == v2) ? options.fn(this) : options.inverse(this);
+                case '===':
+                    return (v1 === v2) ? options.fn(this) : options.inverse(this);
+                case '!=':
+                    return (v1 != v2) ? options.fn(this) : options.inverse(this);
+                case '<':
+                    return (v1 < v2) ? options.fn(this) : options.inverse(this);
+                case '>':
+                    return (v1 > v2) ? options.fn(this) : options.inverse(this);
+                default:
+                    return options.inverse(this);
+            }
+        }
     }
-  }
 }));
 
 app.set('view engine', 'handlebars');
@@ -38,62 +56,63 @@ app.set('views', './views');
 
 // Rota para consultar usuários
 app.get('/', (req, res) => {
-    res.render('index', {titulo: 'Página inicial'});
+    res.render('index', { titulo: 'Página inicial' });
 });
 
 // Rota para a página "Login"
 app.get('/login', (req, res) => {
-    res.render('login', {titulo: 'Login'});
+    res.render('login', { titulo: 'Login' });
 });
 
 // Rota para a página "Cadastro"
 app.get('/registrar', (req, res) => {
-    res.render('registrar', {titulo: 'Cadastro de Usuário'});
+    res.render('registrar', { titulo: 'Cadastro de Usuário' });
 });
 
 // Rota para a página "Sobre"
-app.get('/sobre', (req, res) => { 
-    res.render('sobre', {titulo: 'Sobre o Projeto'});
+app.get('/sobre', (req, res) => {
+    res.render('sobre', { titulo: 'Sobre o Projeto' });
 });
 
 // Rota para a página "Contato"
 app.get('/contato', (req, res) => {
-    res.render('contato', {titulo: 'Fale Conosco'});
+    res.render('contato', { titulo: 'Fale Conosco' });
 });
 
 // Rota para a página Adugo
-app.get('/adugo', (req, res) => { 
-    res.render('adugo', {titulo: 'Adugo (Brasil)'});
+app.get('/adugo', (req, res) => {
+    res.render('adugo', { titulo: 'Adugo (Brasil)' });
 });
 
 // Rota para o dashboard
-app.get('/dashboard', (req, res) => { 
+app.get('/dashboard', (req, res) => {
     const sql = `
     SELECT j.nome_jogo, COUNT(l.ID_log_acesso_de_usuario) AS total
     FROM tb_log_acesso_usuarios l
         JOIN tb_jogos j ON j.ID_jogo = l.fk_jogo
         GROUP BY j.nome_jogo
     `;
-    conexao.query(sql, function(erro, resultados) {
+    conexao.query(sql, function (erro, resultados) {
         if (erro) {
             console.error('Erro ao consultar dashboard:', erro);
             res.status(500).send('Erro ao consultar dashboard');
             return;
         }
-        res.render('dashboard', {titulo: 'Dashboard', dados: resultados});
+        res.render('dashboard', { titulo: 'Dashboard', dados: resultados });
     });
 });
 
 // Rota para consultar usuários
-app.get('/admin/usuarios', (req, res) => {; 
+app.get('/admin/usuarios', (req, res) => {
+    ;
     let sql = 'SELECT * FROM tb_usuarios';
-    conexao.query(sql, function(erro, usuarios_qs) {
+    conexao.query(sql, function (erro, usuarios_qs) {
         if (erro) {
             console.error('Erro ao consultar usuarios:', erro);
             res.status(500).send('Erro ao consultar usuarios');
             return;
         }
-        res.render('usuarios', {usuarios: usuarios_qs});
+        res.render('usuarios', { usuarios: usuarios_qs });
     });
 });
 
@@ -101,7 +120,7 @@ app.get('/admin/usuarios', (req, res) => {;
 app.get('/admin/usuarios/detalhes/:id', (req, res) => {
     const id_usuario = req.params.id;
     const sql = 'SELECT * FROM tb_usuarios WHERE ID_usuario = ?';
-    conexao.query(sql, [id_usuario], function(erro, resultados) {
+    conexao.query(sql, [id_usuario], function (erro, resultados) {
         if (erro) {
             console.error('Erro ao consultar detalhes do usuário:', erro);
             res.status(500).send('Erro ao consultar detalhes do usuário');
@@ -111,7 +130,7 @@ app.get('/admin/usuarios/detalhes/:id', (req, res) => {
             res.status(404).send('Usuário não encontrado');
             return;
         }
-        res.render('detalhes_usuario', {titulo: 'Detalhes do Usuário', usuario: resultados[0], formAction: `/admin/usuarios/detalhes/${id_usuario}`});
+        res.render('detalhes_usuario', { titulo: 'Detalhes do Usuário', usuario: resultados[0], formAction: `/admin/usuarios/detalhes/${id_usuario}` });
     });
 });
 
@@ -119,7 +138,7 @@ app.get('/admin/usuarios/detalhes/:id', (req, res) => {
 app.get('/admin/usuarios/detalhes/:id/editar', (req, res) => {
     const id_usuario = req.params.id;
     const sql = 'SELECT * FROM tb_usuarios WHERE ID_usuario = ?';
-    conexao.query(sql, [id_usuario], function(erro, resultados) {
+    conexao.query(sql, [id_usuario], function (erro, resultados) {
         if (erro) {
             console.error('Erro ao consultar detalhes do usuário:', erro);
             res.status(500).send('Erro ao consultar detalhes do usuário');
@@ -129,7 +148,7 @@ app.get('/admin/usuarios/detalhes/:id/editar', (req, res) => {
             res.status(404).send('Usuário não encontrado');
             return;
         }
-        res.render('editar_usuario', {titulo: 'Editar Usuário', usuario: resultados[0], formAction: `/admin/usuarios/detalhes/${id_usuario}/editar`});
+        res.render('editar_usuario', { titulo: 'Editar Usuário', usuario: resultados[0], formAction: `/admin/usuarios/detalhes/${id_usuario}/editar` });
     });
 });
 
@@ -142,7 +161,7 @@ app.post('/admin/usuarios/detalhes/:id/editar', (req, res) => {
         SET nome_usuario = ?, email_usuario = ?, senha_usuario = ?, telefone_usuario = ?, data_nascimento = ?, genero_usuario = ?, status_usuario = ?, tipo_usuario = ?
         WHERE ID_usuario = ?
     `;
-    conexao.query(sql, [nome_usuario, email_usuario, senha_usuario, telefone_usuario, data_nascimento, genero_usuario, status_usuario, tipo_usuario, id_usuario], function(erro) {
+    conexao.query(sql, [nome_usuario, email_usuario, senha_usuario, telefone_usuario, data_nascimento, genero_usuario, status_usuario, tipo_usuario, id_usuario], function (erro) {
         if (erro) {
             console.error('Erro ao atualizar usuário:', erro);
             res.status(500).send('Erro ao atualizar usuário');
@@ -155,14 +174,14 @@ app.post('/admin/usuarios/detalhes/:id/editar', (req, res) => {
 
 // Rota para adicionar um novo usuário
 app.get('/admin/usuarios/adicionar', (req, res) => {
-    res.render('adicionar_usuario', {titulo: 'Adicionar Usuário', editar_usuario: false, formAction: '/admin/usuarios/adicionar'});
+    res.render('adicionar_usuario', { titulo: 'Adicionar Usuário', editar_usuario: false, formAction: '/admin/usuarios/adicionar' });
 });
 
 // Rota para processar o formulário de adição de usuário
 app.post('/admin/usuarios/adicionar', (req, res) => {
     const { nome_usuario, email_usuario, senha_usuario, tipo_usuario } = req.body;
     const sql = 'INSERT INTO tb_usuarios (nome_usuario, email_usuario, senha_usuario, tipo_usuario) VALUES (?, ?, ?, ?)';
-    conexao.query(sql, [nome_usuario, email_usuario, senha_usuario, tipo_usuario], function(erro) {
+    conexao.query(sql, [nome_usuario, email_usuario, senha_usuario, tipo_usuario], function (erro) {
         if (erro) {
             console.error('Erro ao inserir usuário:', erro);
             res.status(500).send('Erro ao inserir usuário');
@@ -177,7 +196,7 @@ app.post('/admin/usuarios/adicionar', (req, res) => {
 app.post('/admin/usuarios/excluir/:id', (req, res) => {
     const id_usuario = req.params.id;
     const sql = 'DELETE FROM tb_usuarios WHERE ID_usuario = ?';
-    conexao.query(sql, [id_usuario], function(erro) {
+    conexao.query(sql, [id_usuario], function (erro) {
         if (erro) {
             console.error('Erro ao excluir usuário:', erro);
             res.status(500).send('Erro ao excluir usuário');
@@ -207,7 +226,7 @@ app.get('/admin/usuarios/pesquisar', (req, res) => {
         WHERE nome_usuario LIKE ? OR email_usuario LIKE ? OR apelido_usuario LIKE ?
     `;
     const termoPesquisa = `%${termo}%`;
-    conexao.query(sql, [termoPesquisa, termoPesquisa, termoPesquisa], function(erro, usuarios_qs) {
+    conexao.query(sql, [termoPesquisa, termoPesquisa, termoPesquisa], function (erro, usuarios_qs) {
         if (erro) {
             console.error('Erro ao pesquisar usuários:', erro);
             return res.status(500).send('Erro ao pesquisar usuários');
@@ -217,7 +236,7 @@ app.get('/admin/usuarios/pesquisar', (req, res) => {
 });
 
 // Rota para consultar logs de acesso
-app.get('/admin/logs', (req, res) => { 
+app.get('/admin/logs', (req, res) => {
     const sql = `
         SELECT l.ID_log_acesso_de_usuario, u.nome_usuario, j.nome_jogo, l.email_tentado, l.status_acesso, l.ip_origem, l.data_hora
         FROM tb_log_acesso_usuarios l
@@ -225,26 +244,26 @@ app.get('/admin/logs', (req, res) => {
         JOIN tb_jogos j ON j.ID_jogo = l.fk_jogo
         ORDER BY l.data_hora DESC
     `;
-    conexao.query(sql, function(erro, logs_qs) {
+    conexao.query(sql, function (erro, logs_qs) {
         if (erro) {
             console.error('Erro ao consultar logs:', erro);
             res.status(500).send('Erro ao consultar logs');
             return;
         }
-        res.render('logs', {titulo: 'Logs de Acesso', logs: logs_qs});
+        res.render('logs', { titulo: 'Logs de Acesso', logs: logs_qs });
     });
 });
 
 // Rota para consultar jogos
-app.get('/jogos', (req, res) => { 
+app.get('/jogos', (req, res) => {
     const sql = 'SELECT * FROM tb_jogos';
-    conexao.query(sql, function(erro, jogos_qs) {
+    conexao.query(sql, function (erro, jogos_qs) {
         if (erro) {
             console.error('Erro ao consultar jogos:', erro);
             res.status(500).send('Erro ao consultar jogos');
             return;
-        } 
-        res.render('jogos', {jogos: jogos_qs});
+        }
+        res.render('jogos', { jogos: jogos_qs });
     });
 });
 
@@ -252,7 +271,7 @@ app.get('/jogos', (req, res) => {
 app.post('/logout', (req, res) => {
     const sql = 'UPDATE tb_usuarios SET status_usuario = "offline" WHERE ID_usuario = ?';
     const id_usuario = req.body.id_usuario; // Supondo que o ID do usuário esteja sendo enviado no corpo da requisição
-    conexao.query(sql, [id_usuario], function(erro) {
+    conexao.query(sql, [id_usuario], function (erro) {
         if (erro) {
             console.error('Erro ao atualizar status do usuário:', erro);
             res.status(500).send('Erro ao atualizar status do usuário');
@@ -260,7 +279,7 @@ app.post('/logout', (req, res) => {
         }
         console.log('Status do usuário atualizado para offline com sucesso!');
     });
-    conexao.end(function(erro) {
+    conexao.end(function (erro) {
         if (erro) {
             console.error('Erro ao encerrar a conexão com o banco de dados:', erro);
             res.status(500).send('Erro ao encerrar a conexão com o banco de dados');
@@ -274,3 +293,5 @@ app.post('/logout', (req, res) => {
 });
 
 app.listen(8080); // Inicia o servidor na porta 8080
+
+module.exports = app.js; // Exporta o app para testes ou outros usos
